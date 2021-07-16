824d5f84ac509127c9bb7f6bb1fbb7f1cd016ab6
dmi3eva <forkatdmi@gmail.com>
2021-07-16 11:57:51 +0300

    Codeforces 111-C

--- codeforces/111_C.py
+++ codeforces/111_C.py
@@ -1,5 +1,24 @@
-def solve(numbers):
-    pass
+def extract_identical(numbers):
+    coordinates = {}
+    identicals = []
+    for _ind, _num in enumerate(numbers):
+        if _num in coordinates.keys():
+            identicals.append((coordinates[_num][-1], _ind))
+            print(_num)
+            print(identicals)
+        coordinates[_num] = coordinates.get(_num, []) + [_ind]
+    print(identicals)
+    return identicals
+
+
+
+def solve(a):
+    identical_numbers = extract_identical(a)
 
 
 t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = map(int, input().strip().split())
+    answer = solve(a)
+    print(extract_identical(a))
