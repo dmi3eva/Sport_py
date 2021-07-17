a4d42768ad5fb888809ae5292a4b2eafdcd558af
dmi3eva <forkatdmi@gmail.com>
2021-07-17 12:12:17 +0300

    Codeforces 111-C (editorial is corrrect, my is not)

--- codeforces/111_C.py
+++ codeforces/111_C.py
@@ -29,7 +29,7 @@ def extract_segments(numbers):
     return segments
 
 
-def solve(a):
+def my_solution(a):
     identicals = extract_segments(a) + [(len(a) - 1, len(a))]
     subarray_amount = 0
     s_ind = 0
@@ -44,9 +44,39 @@ def solve(a):
         s_ind = a + 1
     return subarray_amount
 
+
+def triple_is_bad(a0, a1, a2):
+    increasing = a0 >= a1 >= a2
+    decreasing = a0 <= a1 <= a2
+    return increasing or decreasing
+
+
+def four_is_bad(segment):
+    first = triple_is_bad(segment[0], segment[1], segment[3])
+    second = triple_is_bad(segment[0], segment[2], segment[3])
+    third = triple_is_bad(segment[1], segment[2], segment[3])
+    return first or second or third
+
+def editorial_solution(a):
+    amount = 0
+    for ind, number in enumerate(a):
+        amount += 1
+        if ind < len(a) - 1:
+            amount += 1
+        if ind < len(a) - 2 and not triple_is_bad(a[ind], a[ind + 1], a[ind + 2]):
+            amount += 1
+        else:
+            continue
+        if ind < len(a) - 3 and not four_is_bad(a[ind : ind + 4]):
+            amount += 1
+    return amount
+
+
+
 t = int(input())
 for _ in range(t):
     n = int(input())
     a = list(map(int, input().strip().split()))
-    answer = solve(a)
+    # answer = my_solution(a)
+    answer = editorial_solution(a)
     print(answer)
