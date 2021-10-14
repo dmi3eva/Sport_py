5e4c37b6efa61aa245300997dcfcf4e22a357957
dmi3eva <forkatdmi@gmail.com>
2021-10-14 18:46:02 +0300

    5P-October, part 2

---
+++ codeforces/1175_A.py
@@ -0,0 +1,14 @@
+def count_steps(n, k):
+    steps_num = 0
+    while n > 0:
+        while n % k == 0 and n > 0:
+            n //= k
+            steps_num += 1
+        steps_num += n % k
+        n -= n % k
+    return steps_num
+
+req = int(input())
+for _ in range(req):
+    n, k = map(int, input().strip().split())
+    print(count_steps(n, k))
