6dd4c64d6bc59ee61d6e461fe69ed129dc8fae03
dmi3eva <forkatdmi@gmail.com>
2022-03-14 16:17:51 +0300

    ADD: 5P_2_07 (quiz)

---
+++ informatics/84.py
@@ -0,0 +1,18 @@
+def verify(sequence):
+    output_line = ''.join(sequence)
+    print(output_line)
+
+
+def generate(sequence, k, position):
+    if position >= len(sequence) and k == 0:
+        verify(sequence)
+    elif 0 <= k <= len(sequence) - position:
+        sequence[position] = '0'
+        generate(sequence, k, position + 1)
+        sequence[position] = '1'
+        generate(sequence, k - 1, position + 1)
+
+
+n, k = [int(x) for x in input().strip().split(' ')]
+sequence = ['0' for _ in range(n)]
+generate(sequence, k, 0)
