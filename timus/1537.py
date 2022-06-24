859df34a63f88066863006cbd5c4a4c953a3d366
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-06-24 11:04:44 +0300

    FIX: 1537

--- timus/1537.py
+++ timus/1537.py
@@ -5,17 +5,21 @@ from typing import *
 sys.setrecursionlimit(1000)
 
 
-def count_ents(start_amount: int, k: int, p: int, height: int) -> int:
+def count_ents(start_amount: int, k: int, p: int) -> int:
     if start_amount % p == k:
-    #     from_old = count_ents(start_amount + 1, k, p, 0)
-    #     from_yang = count_ents(start_amount * 2, k, p, 0)
-        return 1  # + from_old + from_yang
-    # if height > p:
-    if start_amount > min(k, p):
+        return 1
+    if start_amount > k:
         return 0
-    from_old = count_ents(start_amount + 1, k, p, height + 1)
-    from_yang = count_ents(start_amount * 2, k, p, height + 1)
-    return from_old + from_yang
+    from_old = count_ents(start_amount + 1, k, p)
+    from_yang = count_ents(start_amount * 2, k, p)
+    return (from_old + from_yang) % p
+
+# # Восходящая
+# dp[2] = 1
+# for i in range(k + 1):
+#     dp[i * 2] =
+#
+# # Нисходящая
 
 
 k, p = map(int, input().split())
