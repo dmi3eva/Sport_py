fc0442037eb13eaa16394522cfc7768eb9bbf573
forkatdmi@gmail.com <forkatdmi@gmail.com>
2022-07-13 19:51:20 +0300

    FIX: informatics-766, remove partitions

--- informatics/766.py
+++ informatics/766.py
@@ -1,33 +1,23 @@
 from typing import *
 
 
-def partition(numbers: List[int], left: int, right: int) -> List[int]:
-    pivot_ind = (left + right) // 2
-    pivot_value = numbers[pivot_ind]
-    less = left
-    greater = right - 1
-    while less < greater:
-        while numbers[less] < pivot_value:
-            less += 1
-        while numbers[greater] > pivot_value:
-            greater -= 1
-        numbers[less], numbers[greater] = numbers[greater], numbers[less]
-    return numbers
 
 
-def quick_sort(numbers: List[int], left: int, right: int) -> List[int]:
-    if right - left <= 1:
+def quick_sort(numbers: List[int]) -> List[int]:
+    if len(numbers) <= 1:
         return numbers
-    pivot_ind = (left + right) // 2
-    numbers = partition(numbers, left, right)
-    numbers = quick_sort(numbers, left, pivot_ind)
-    numbers = quick_sort(numbers, pivot_ind, right)
-    return numbers
+    pivot_ind = len(numbers) // 2
+    pivot_value = numbers[pivot_ind]
+    less = [_n for _n in numbers if _n < pivot_value]
+    greater = [_n for _n in numbers if _n > pivot_value]
+    less = quick_sort(less)
+    greater = quick_sort(greater)
+    return less + [pivot_value] + greater
 
 
 if __name__ == "__main__":
     n = int(input())
     values = list(map(int, input().split()))
-    sorted_values = quick_sort(values, 0, len(values))
+    sorted_values = quick_sort(values)
     for _value in sorted_values:
         print(_value, end=' ')
